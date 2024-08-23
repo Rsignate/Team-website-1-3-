f7530b02cbcd1536e33b5c4a28209d5bdc52dbe6
Rsignate <122956000+Rsignate@users.noreply.github.com>
2024-08-22 21:03:04 -0400

    Add files via upload

---
+++ Ramatoulaye_Signate.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+	<title> <?= "Ramatoulaye resume" ?> </title>
+
+    
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Your name's resume">
+    <meta name="author" content="Your name">    
+    <link rel="shortcut icon" href="favicon.ico"> 
+    
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+    
+    <!-- FontAwesome JS-->
+	<script defer src="assets/fontawesome/js/all.min.js"></script>
+       
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/pillar-1.css">
+
+
+</head> 
+
+<body>
+    <article class="resume-wrapper text-center position-relative">
+	    <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
+		    <header class="resume-header pt-4 pt-md-0">
+			    <div class="row">
+				    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+				        <img class="picture" src="assets/images/profile.jfif" alt="">
+				    </div><!--//col-->
+				    <div class="col">
+					    <div class="row p-4 justify-content-center justify-content-md-between">
+						    <div class="primary-info col-auto">
+								
+							   <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase"><?= "Rama Signate"?></h1>
+							   <div class="title mb-3"><?= "Software Developer" ?></div>
+							    <ul class="list-unstyled">
+								    <li class="mb-2"><a class="text-link" href="#"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i><?= "signater1@nku.edu" ?></a></li>
+								    <li><a class="text-link" href="#"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i><?= "3478053318"?></a></li>
+							    </ul>
+						    </div><!--//primary-info-->
+						    <div class="secondary-info col-auto mt-2">
+							    <ul class="resume-social list-unstyled">
+					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span><?= "https://www.linkedin.com/in/ramatoulayesignate/"?></a></li>
+					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span><?="https://github.com/Rsignate"?></a></li>
+					                <li><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fas fa-globe"></i></span><?="https://github.com/Rsignate/Mywebpage"?></a></li>
+							    </ul>
+						    </div><!--//secondary-info-->
+					    </div><!--//row-->
+					    
+				    </div><!--//col-->
+			    </div><!--//row-->
+		    </header>
+		    <div class="resume-body p-5">
+			    <section class="resume-section summary-section mb-5">
+				    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Summary</h2>
+				    <div class="resume-section-content">
+					    <p class="mb-0"><?="As an IT enthusiast with a passion for innovation, I am driven to deliver high-quality results while constantly seeking to develop my skills and knowledge. With my proven ability to adapt to changing priorities and environments, I am always ready to take on new challenges and collaborate effectively with colleagues at all levels. Whether working on complex projects or tackling routine tasks, I bring a dedicated, detail-oriented approach to everything I do. If you're looking for a dynamic team player with a strong work ethic and a commitment to excellence, let's connect and explore how we can work together to achieve your goals."?></p>
+				    </div>
+			    </section><!--//summary-section-->
+			    <div class="row">
+				    <div class="col-lg-9">
+					    <section class="resume-section experience-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Work Experience</h2>
+						    <div class="resume-section-content">
+							    <div class="resume-timeline position-relative">
+								    <article class="resume-timeline-item position-relative pb-5">
+									    
+									    <div class="resume-timeline-item-header mb-2">
+										    <div class="d-flex flex-column flex-md-row">
+										        <h3 class="resume-position-title font-weight-bold mb-1"><?="Cashier, Bookseller"?></h3>
+										        <div class="resume-company-name ms-auto"><?="Barners and Noble"?></div>
+										    </div><!--//row-->
+										    <div class="resume-position-time"><?= "Jan 2020 - Nov 2020" ?></div>
+									    </div><!--//resume-timeline-item-header-->
+									    <div class="resume-timeline-item-desc">
+						
+										    <h4 class="resume-timeline-item-desc-heading font-weight-bold">Achievements:</h4>
+										  
+										    <ul>
+											    <li><?= "Assisted approximately 50 customers daily with inquiries and cash transactions."?></li>
+											    <li><?="Resolved customer complaints with efficiency and professionalism, receiving recognition from senior management for
+maintaining customer satisfaction ratings above 95%."?></li>
+											    <li><?="Maintained a well-organized sales floor for optimal customer shopping experience."?></li>
+											    
+										    </ul>
+							    
+							    
+							    
+						    </div>
+					    </section><!--//projects-section-->
+				    </div>
+				    <div class="col-lg-3">
+					    <section class="resume-section skills-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Skills &amp; Tools</h2>
+						    <div class="resume-section-content">
+						        <div class="resume-skill-item">
+							        <ul class="list-unstyled mb-4">
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?="Java"?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 98%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?="Python"?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 94%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?="JavaScript"?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 96%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?="SQL"?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 92%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+								        <li class="mb-2">
+								            <div class="resume-skill-name"><?="HTML/CSS"?></div>
+									        <div class="progress resume-progress">
+											    <div class="progress-bar theme-progress-bar-dark" role="progressbar" style="width: 96%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+								        </li>
+							        </ul>
+						        </div><!--//resume-skill-item-->
+						        <div class="resume-skill-item">
+						            <h4 class="resume-skills-cat font-weight-bold">Others</h4>
+						            <ul class="list-inline">
+							            <li class="list-inline-item"><span class="badge badge-light"><?="Project Management"?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?="Troubleshooting"?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?="linux/unix"?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?=" Computer programming"?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?="Risk Management"?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?="WordPress"?></span></li>
+							            <li class="list-inline-item"><span class="badge badge-light"><?="Tableau"?></span></li>
+						            </ul>
+						        </div><!--//resume-skill-item-->
+						    </div><!--resume-section-content-->
+					    </section><!--//skills-section-->
+					    <section class="resume-section education-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Education</h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled">
+								    <li class="mb-2">
+								        <div class="resume-degree font-weight-bold"><?="BS in Computer information technology"?></div>
+								        <div class="resume-degree-org"><?"Northern Kentucky University"?></div>
+								        <div class="resume-degree-time"><?"2020 - 2024"?></div>
+								    </li>
+								   
+							    </ul>
+						    </div>
+					    </section><!--//education-section-->
+					    <section class="resume-section reference-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Awards</h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled resume-awards-list">
+								    <li class="mb-2 ps-4 position-relative">
+								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
+								        <div class="resume-award-name"><?="Edge Award"?></div>
+								        <div class="resume-award-desc"><?="It is designed to recognize academically outstanding and creatively talented students. The University awards scholarships to highly motivated students who demonstrate strong academic performance."?></div>
+								    </li>
+								    <li class="mb-0 ps-4 position-relative">
+								        <i class="resume-award-icon fas fa-trophy position-absolute" data-fa-transform="shrink-2"></i>
+								        <div class="resume-award-name"><?="Dean's List"?></div>
+								        <div class="resume-award-desc"><?="Recognized for exceptional academic performance and dedication, demonstrating a high level of commitment and excellence in my studies."?></div>
+								    </li>
+							    </ul>
+						    </div>
+					    </section><!--//interests-section-->
+					    <section class="resume-section language-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Languages</h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled resume-lang-list">
+								    <li class="mb-2"><span class="resume-lang-name font-weight-bold"><?="French"?></span> <small class="text-muted font-weight-normal">(Native)</small></li>
+								    <li class="mb-2 align-middle"><span class="resume-lang-name font-weight-bold"><?="English"?></span> <small class="text-muted font-weight-normal">(Professional)</small></li>
+								    <li><span class="resume-lang-name font-weight-bold"><?="wolof"?></span> <small class="text-muted font-weight-normal">(Professional)</small></li>
+							    </ul>
+						    </div>
+					    </section><!--//language-section-->
+					    <section class="resume-section interests-section mb-5">
+						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Interests</h2>
+						    <div class="resume-section-content">
+							    <ul class="list-unstyled">
+								    <li class="mb-1"><?="swimming"?></li>
+								    <li class="mb-1"><?="Shopping"?></li>
+								    <li class="mb-1"><?="Cooking"?></li>
+							    </ul>
+						    </div>
+					    </section><!--//interests-section-->
+					    
+				    </div>
+			    </div><!--//row-->
+				<section class="resume-section experience-section mb-5">
+					<h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Projects</h2>
+					<div class="row mt-4">
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image1.jpg" alt="Project 1" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title"><?="Designed a Money Transfer Web Application"?></h5>
+									<p class="card-text"><?="Led a team of 3 in developing a secure, fee-free Money Transfer Web Application that facilitates seamless transactions for
+users worldwide.Designed and implemented intuitive features including registration/login capabilities, transaction management systems, and
+automated fund confirmation to optimize user experience.Implemented advanced encryption protocols to secure user data and transactions globally, collaborating cross-functionally to
+enhance application functionality and meet evolving industry standards."?></p>
+									<a href="btn btn-outline-primary" href="#"><?="https://github.com/CKORDA/MoneyTransfer----Midterm"?></a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card">
+								<img src="path-to-project-image2.jpg" alt="Project 2" class="card-img-top">
+								<div class="card-body">
+									<h5 class="card-title"><?="Created a Dynamic API-Driven eCommerce Website"?></h5>
+									<p class="card-text"><?="Built a responsive eCommerce website using HTML, CSS, and JavaScript. The site includes filters for categories like books,
+music, games, shoes, and furniture. Integrated an intuitive search feature that allows users to see items with images, descriptions, and prices, making browsing
+and buying smoother and more engaging."?>.</p>
+									<a href="btn btn-outline-primary" href="#"><?="https://github.com/Rsignate/dynamic-API-driven-eCommerce-website"?></a>
+								</div>
+							</div>
+						</div>
+						
+							</div>
+						</div>
+					</div>
+				</section><!--//projects-section-->
+		    </div><!--//resume-body-->
+		    
+		    
+	    </div>
+    </article> 
+
+    
+    <footer class="footer text-center pt-2 pb-5">
+	    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+        <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> <?="by Ramatoulaye"?></small>
+    </footer>
+
+    
+
+</body>
+</html> 
+
